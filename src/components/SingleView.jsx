44183354326d1288ea788222756fc3e208241467
nealukumies <nea.lukumies@metropolia.fi>
2025-04-07 15:00:46 +0300

    feat: implement media display logic in SingleView component and enhance styling in index.css

--- src/components/SingleView.jsx
+++ src/components/SingleView.jsx
@@ -1,16 +1,14 @@
 const SingleView = (props) => {
   const {item, setSelectedItem} = props;
   return (
-    // TODO: Add JSX for displaying a mediafile here
-    // - use e.g. a <dialog> element for creating a modal
-    // - use item prop to render the media item details
-    // - use img tag for displaying images
-    // - use video tag for displaying videos
-
     <>
       {item && (
         <dialog open>
-          <img src={item.filename} alt={item.title} />
+          {item.media_type.includes('video') ? (
+            <video src={item.filename} controls />
+          ) : (
+            <img src={item.filename} alt={item.title} />
+          )}
           <h3>Title: {item.title}</h3>
           <p>Description: {item.description}</p>
           <button onClick={() => setSelectedItem(null)}>Close</button>
