f6d58618a8a35687be2db8a890abb911f5d8fa05
nealukumies <nea.lukumies@metropolia.fi>
2025-04-07 14:34:54 +0300

    feat: add media selection functionality in Home and MediaRow components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,6 @@
+import {useState} from 'react';
 import MediaRow from './MediaRow';
+
 const mediaArray = [
   {
     media_id: 8,
@@ -37,6 +39,8 @@ const mediaArray = [
 ];
 
 const Home = () => {
+  const [selectedItem, setSelectedItem] = useState(null);
+  console.log('selected item ', selectedItem);
   return (
     <>
       <h2>My Media</h2>
@@ -49,11 +53,16 @@ const Home = () => {
             <th>Created</th>
             <th>Size</th>
             <th>Type</th>
+            <th>Operations</th>
           </tr>
         </thead>
         <tbody>
           {mediaArray.map((item) => (
-            <MediaRow key={item.media_id} item={item} />
+            <MediaRow
+              key={item.media_id}
+              item={item}
+              setSelectedItem={setSelectedItem}
+            />
           ))}
         </tbody>
       </table>
--- src/components/MediaRow.jsx
+++ src/components/MediaRow.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 const MediaRow = (props) => {
-  const {item} = props;
+  const {item, setSelectedItem} = props;
   return (
     <tr key={item.media_id}>
       <td>
@@ -12,12 +12,16 @@ const MediaRow = (props) => {
       <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
       <td>{item.filesize}</td>
       <td>{item.media_type}</td>
+      <td>
+        <button onClick={setSelectedItem(item)}>View</button>
+      </td>
     </tr>
   );
 };
 
 MediaRow.propTypes = {
   item: PropTypes.object.isRequired,
+  setMediaItem: PropTypes.func.isRequired,
 };
 
 export default MediaRow;
